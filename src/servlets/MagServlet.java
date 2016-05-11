45bfb1467fd535f3d435c2a320edf79662f3b7d6
xcr_abcd <530551426@qq.com>
2016-05-11 14:24:14 +0800

    1-hop commit

--- src/servlets/MagServlet.java
+++ src/servlets/MagServlet.java
@@ -8,7 +8,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.client.HttpClient;
+
+import common.Conf;
+import common.QueryString;
+import entity.Author;
+import entity.EvaluateResult;
+import entity.Field;
+import entity.Paper;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import request.Evaluate;
+import utils.HttpUtils;
+
 public class MagServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5870121637506507847L;
 
 	/**
 	 * Constructor of the object.
@@ -16,13 +34,17 @@ public class MagServlet extends HttpServlet {
 	public MagServlet() {
 		super();
 	}
+	
+	private HttpClient httpClient;
 
 	/**
 	 * Destruction of the servlet. <br>
 	 */
 	public void destroy() {
-		super.destroy(); // Just puts "destroy" string in log
-		// Put your code here
+		super.destroy(); 
+		
+		jedisPool.close();
+		
 	}
 
 	/**
@@ -37,29 +59,91 @@ public class MagServlet extends HttpServlet {
 	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		long a = System.currentTimeMillis();
 
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the GET method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+		Long id1 = Long.valueOf(request.getParameter("id1"));
+		Long id2 = Long.valueOf(request.getParameter("id2"));
+		
+		
+		
+		
+		//对id1,id2进行预处理，判断它们的id类型，再开线程，处理它们的hop关系。
+		Jedis jedis = jedisPool.getResource();
+		
+		String type1 = jedis.get(id1+"T");
+		String type2 = jedis.get(id2+"T");
+		if(type1==null||"nil".equals(type1)||type2==null||"nil".equals(type2)){
+			Evaluate evaluate = new Evaluate();
+			evaluate.setExpr(String.format(QueryString.eval_judge,id1,id2));
+			evaluate.setCount(5000);
+			evaluate.setOffset(0);
+			evaluate.setAttributes("Id,F.FId,C.CId,J.JId,AA.AuId,AA.AfId");
+			String res = evaluate.doRequest(httpClient);
+			EvaluateResult result = EvaluateResult.parse(res);
+			for(Paper entity:result.getEntities()){
+				String id = entity.getId().toString();
+				final String v1 = id+"T",v2=id+"R";
+				
+				if(entity.getAuthors()==null){
+					jedis.set(v1, "author");
+					
+				}else{
+					jedis.set(v1, "paper");
+					if(entity.getFields()!=null){
+						for(Field field:entity.getFields()){
+							jedis.sadd(v2, field.getId().toString());
+						}
+					}
+					
+					if(entity.getConference()!=null){
+						jedis.sadd(v2, entity.getConference().getId().toString());
+					}
+					if(entity.getJournal()!=null){
+						jedis.sadd(v2, entity.getJournal().getId().toString());
+					}
+					if(entity.getAuthors()!=null){
+						for(Author author:entity.getAuthors()){
+							jedis.sadd(v2, author.getId().toString(),author.getAffiliationId().toString());
+							
+							
+						}
+					}
+				}
+				
+			}
+		}
+		
+		
+	
+		
+		PrintWriter writer = response.getWriter();
+		HopResult hopResult = new HopResult();
+		if(OneHopTest.test(id1, id2, jedis)){
+			
+			hopResult.put(id1,id2);
+		}
+		
+		
+		
+		writer.write(hopResult.toJSONArray());
+		writer.close();
+		
+		long b = System.currentTimeMillis();
+		
+		System.out.println("Servlet mag time:"+(b-a)+"ms.");
+		
+		
+		
+		
 	}
-
-	/**
-	 * Initialization of the servlet. <br>
-	 *
-	 * @throws ServletException if an error occurs
-	 */
+	
+	
+	private JedisPool jedisPool;
+	
 	public void init() throws ServletException {
-		// Put your code here
+		jedisPool = new JedisPool(Conf.redis_ip,Integer.valueOf(Conf.redis_port));
+		httpClient = HttpUtils.client();
 	}
 
 }
